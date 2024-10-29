b4eba86425256949d7885fd27d34d90a49204bd4
Paul Tiberghien <paultiberghien1@gmail.com>
2024-10-29 16:03:33 -0700

    update colour change

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const body = document.body;
 
 // add click event listener to toggle the background color
 toggleButton[0].addEventListener('click', () => {
-    body.classList.toggle('pastel-blue'); // toggle the pastel blue class
+    body.classList.toggle('spotify-purple'); // toggle the pastel blue class
 });
