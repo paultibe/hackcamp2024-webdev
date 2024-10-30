374640862df76138b5a838a80cefe79359c93e15
Paul Tiberghien <paultiberghien1@gmail.com>
2024-10-29 19:23:58 -0700

    small fixes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// select the toggle button and body
 const toggleButton = document.getElementsByClassName('toggle-button');
 const body = document.body;
 
-// add click event listener to toggle the background color
 toggleButton[0].addEventListener('click', () => {
-    body.classList.toggle('spotify-purple'); // toggle the pastel blue class
+    body.classList.toggle('spotify-purple'); 
 });
